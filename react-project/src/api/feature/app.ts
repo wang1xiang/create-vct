d2edce67609306532d3e87f0a064a7958a107978
王翔 <wangxiang@qimingpian.com>
2023-05-12 09:30:11 +0800

    featiew模块接口对接&联调

--- react-project/src/api/feature/app.ts
+++
@@ -1,9 +0,0 @@
-import { GithubType } from '../interface';
-import request from '../request';
-
-/* 用户列表 */
-export const getUserList = (searchName: string) =>
-  request<GithubType>({
-    url: `/api/search/users?q=${searchName}`,
-    method: 'get',
-  });
